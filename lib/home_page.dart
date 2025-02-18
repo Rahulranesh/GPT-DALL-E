50d928634813099b09385d18e1700d04d00d4941
ranesh <rahulranesh590@gmail.com>
2025-02-18 23:18:02 +0530

    build errors !

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -106,7 +106,7 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ],
-              ),
+              )
             ),
 
             // Chat bubble
